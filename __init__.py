3e38780f06a720427cf5677561b247cec75a7b21
Aaron Fu Lei <aaron.fu@alumni.ust.hk>
2024-10-13 08:11:30 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,6 +1,9 @@
-from .empty_selection.plugin import EmptySelectionCommand
-from .show_at_top_or_center.plugin import ShowAtTopOrCenterCommand
-from .show_at_top_or_center.plugin import ShowAtTopOrCenterListener
+from .shrink_selection_to_caret.plugin import ShrinkSelectionToCaretCommand
+
+from .show_at_top_or_center.plugin import (
+    ShowAtTopOrCenterCommand,
+    ShowAtTopOrCenterListener)
+
 from .show_git_blame_in_status_bar.plugin import (
     ShowGitBlameInStatusBarListener
 )
