fab1a4eec5cebd673721e850a6cd4571d9f30bd5
Aaron Fu Lei <aaron.fu@alumni.ust.hk>
2024-10-13 08:23:59 +0800

    Simplify ShowAtTopOrCenterCommand

--- __init__.py
+++ __init__.py
@@ -1,8 +1,6 @@
 from .shrink_selection_to_caret.plugin import ShrinkSelectionToCaretCommand
 
-from .show_at_top_or_center.plugin import (
-    ShowAtTopOrCenterCommand,
-    ShowAtTopOrCenterListener)
+from .show_at_top_or_center.plugin import ShowAtTopOrCenterCommand
 
 from .show_git_blame_in_status_bar.plugin import (
     ShowGitBlameInStatusBarListener
