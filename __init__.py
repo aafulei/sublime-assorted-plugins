a54705915d44e140274a868db0a96bc5207bad60
Aaron Fu Lei <aaron.fu@alumni.ust.hk>
2024-10-14 17:40:15 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,4 +1,9 @@
-from .highlight_all_occurences.plugin import HighlightAllOccurencesListener, HighlightAllOccurencesToggleSettingCommand
+from .highlight_all_occurences.plugin import (
+    HighlightAllOccurencesListener, HighlightAllOccurencesToggleSettingCommand)
+
 from .show_at_top_or_center.plugin import ShowAtTopOrCenterCommand
-from .show_git_blame_in_status_bar.plugin import ShowGitBlameInStatusBarListener
+
+from .show_git_blame_in_status_bar.plugin import (
+    ShowGitBlameInStatusBarListener)
+
 from .shrink_selection_to_caret.plugin import ShrinkSelectionToCaretCommand
